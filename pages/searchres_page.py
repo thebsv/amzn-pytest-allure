ecb0b116077389453c7ee20c07ee36ea6bab2a9e
Bhargav Srinivasan <bhargav.srinivasan92@gmail.com>
2022-04-17 19:32:15 +0530

    prints all results

--- pages/searchres_page.py
+++ pages/searchres_page.py
@@ -1,3 +1,5 @@
+from typing import List
+
 from selenium.webdriver.common.by import By
 
 
@@ -7,6 +9,9 @@ class SearchResultsPage:
         self.results_xpath = "//*[@id=\"search\"]/div[1]/div[1]/div/span[3]/div[2]/div[1]/div/span/div/div/span"
         self.first_res_type1 = "//*[@id=\"search\"]/div[1]/div[1]/div/span[3]/div[2]/div[2]"
         self.second_res_type1 = "//*[@id=\"search\"]/div[1]/div[1]/div/span[3]/div[2]/div[3]"
+        self.first_res_type2 = "/html/body/div[1]/div[2]/div[1]/div[1]/div/span[3]/div[2]/div[2]/div/div/div/div/div/div[1]/div/div[2]/div/span/a/div"
+        self.all_items = "//div[@data-component-type='s-search-result']"
+        self.todays_deals = "//*[@id=\"nav-xshop\"]/a[1]"
 
     def click_first_result(self):
         self.driver.find_element(By.XPATH, self.first_res_type1).click()
@@ -17,12 +22,18 @@ class SearchResultsPage:
         self.driver.implicitly_wait(1)
 
     def click_first_result_type2(self):
-        self.driver.find_element(By.XPATH, "/html/body/div[1]/div[2]/div[1]/div[1]/div/span[3]/div[2]/div[2]/div/div/div/div/div/div[1]/div/div[2]/div/span/a/div").click()
+        self.driver.find_element(By.XPATH, self.first_res_type2).click()
         self.driver.implicitly_wait(1)
 
     def click_todays_deals(self):
-        self.driver.find_element(By.XPATH, "//*[@id=\"nav-xshop\"]/a[1]").click()
+        self.driver.find_element(By.XPATH, self.todays_deals).click()
         self.driver.implicitly_wait(1)
+
+    def print_all_items(self) -> List[str]:
+        res = []
+        for elem in self.driver.find_elements(By.XPATH, self.all_items):
+            res.append(elem.text)
+        return res
 
     def verify_results_page(self) -> str:
         return self.driver.find_element(By.XPATH, self.results_xpath).text
--- tests/test_search.py
+++ tests/test_search.py
@@ -16,7 +16,6 @@ class TestSearch:
         home.search_for(item)
         sres = SearchResultsPage(driver)
         assert "RESULTS" == sres.verify_results_page()
-        # detect type of result page
         sres.click_first_result()
         test_name = environment.whoami()
         screenshot_name = test_name + "_screenshot1_"
@@ -45,14 +44,9 @@ class TestSearch:
         home.search_for(item)
         sres = SearchResultsPage(driver)
         assert "RESULTS" == sres.verify_results_page()
-
-        # for elem in driver.find_elements(By.XPATH, "//div[@data-component-type='s-search-result']"):
-        #     print("item: ", elem.text)
-        sres.click_first_result_type2()
-        driver.implicitly_wait(5)
-        item = ItemPage(driver)
-        print("PRICE: ", item.get_item_price())
-        driver.back()
+        print("ITEMS: \n")
+        for item in sres.print_all_items():
+            print(item)
 
     def test_today_deals(self, driver):
         driver.get(environment.URL)

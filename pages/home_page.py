b3d5905db8cdb1adcdb832b3ab200e4c429fe139
Bhargav Srinivasan <bhargav.srinivasan92@gmail.com>
2022-04-18 11:31:03 +0530

    added hover function

--- pages/home_page.py
+++ pages/home_page.py
@@ -1,18 +1,21 @@
 from selenium.webdriver.common.by import By
+from pages.base_page import BasePage
 
 
-class HomePage:
+class HomePage(BasePage):
     def __init__(self, driver):
         self.driver = driver
-        self.sign_in_id = "nav-signin-tooltip"
+        super().__init__(self.driver)
+        self.sign_in_id = "nav-link-accountList"
         self.search_box = "twotabsearchtextbox"
         self.search_id = "nav-search-submit-button"
         self.logout_box = "nav-link-accountList"
         self.logout_button = "https://www.amazon.com/gp/flex/sign-out.html"
 
     def click_signin(self):
-        self.driver.implicitly_wait(3)
-        self.driver.find_element(By.ID, self.sign_in_id).click()
+        self.hover_over_element(self.driver.find_element(By.ID, self.sign_in_id))
+        self.wait_for_element((By.ID, "nav-flyout-ya-signin"))
+        self.driver.find_element(By.ID, "nav-flyout-ya-signin").click()
 
     def search_for(self, item):
         self.driver.find_element(By.ID, self.search_box).send_keys(item)
